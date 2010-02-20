f6f7381483d8d238af4e26363079ea7ef79656c3
jamesu <jamesu@gmail.com>
2010-02-20 14:50:16 +0000

    Add timer

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -42,3 +42,56 @@ Report = {
 		);
   }	
 };
+
+Timer = {
+	instances: null,
+	
+	init: function() {
+		this.instances = new Hash();
+		setInterval(Timer.tick, 1000);
+	},
+	
+	register: function(id) {
+		var el = $('entry_' + id);
+		var time = el.down('.entryTime');
+		var start = time.readAttribute('start_date');
+		this.instances.set(id, [time, Date.parse(start)]);
+	},
+	
+	restart: function(id) {
+		this.remove(id);
+		this.register(id);
+	},
+	
+	remove: function(id) {
+		var inst = this.instances.get(id);
+		if (inst) {
+			inst[0] = null;
+			inst[1] = null;
+			
+			this.instances.unset(id);
+		}
+	},
+	
+	tick: function() {
+		var now = Date.now();
+		Timer.instances.values().forEach(function(inst) {
+			var delta_s = (now - inst[1]) / 1000;
+			var delta_m = delta_s / 60.0;
+			var delta_h = Math.floor(delta_m / 60.0);
+			
+			delta_m = Math.floor(delta_m - (delta_h * 60.0));
+			
+			var delta_desc = "";
+			if (delta_h < 1.0) {
+				if (delta_m < 1.0)
+					delta_desc = delta_h + "H" + delta_m + "M";
+				else
+					delta_desc = delta_m + "M";
+			} else {
+				delta_desc = delta_h + "H" + delta_m + "M";
+			}
+			inst[0].innerHTML = delta_desc;
+		});
+	}
+};
